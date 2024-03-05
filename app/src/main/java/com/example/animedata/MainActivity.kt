bd691ce6c2af9e438f8937def6536a863d79a937
LaelMedina <medinalael10@gmail.com>
2024-03-05 10:27:51 -0600

    feat: Creating the title row

--- app/src/main/java/com/example/animedata/MainActivity.kt
+++ app/src/main/java/com/example/animedata/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.animedata
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.animedata.ui.theme.AnimeDataTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyView()
                 }
             }
         }
@@ -30,17 +37,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeDataTheme {
-        Greeting("Android")
+fun MyView() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Anime List",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Black
+            )
+        )
     }
 }
