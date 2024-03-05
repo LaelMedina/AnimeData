25211952578ced41c3dff102dd779ed51f58a777
LaelMedina <medinalael10@gmail.com>
2024-03-05 10:14:22 -0600

    feat: Addding the Anime Model

---
+++ app/src/main/java/com/example/animedata/Models/Anime.kt
@@ -0,0 +1,9 @@
+package com.example.animedata.Models
+
+data class Anime(
+    val name : String,
+    val chapters : Int,
+    val description: String,
+    val released: String,
+    val author: String
+)
